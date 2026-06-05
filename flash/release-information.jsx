1b5bfe6ec00e5c0a567a7d818541d4a4722508b1
Abin Joseph <abinjose@qti.qualcomm.com>
2026-06-05 14:57:33 +0530

    docs/flash: add doc to obtain prebuilt images
    Prebuilt images are generated for qcom-multimedia-image and
    qcom-multimedia-proprietary-image. These can be directly used for
    flashing instead of compiling the image.
    
    Signed-off-by: Abin Joseph <abinjose@qti.qualcomm.com>

---
+++ flash/release-information.jsx
@@ -0,0 +1,18 @@
+export const prebuilt_urls = {
+  "rb3gen2-core-kit": [
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/rb3gen2-core-kit/qli-2.0-qcom-multimedia-image.zip",
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/rb3gen2-core-kit/qli-2.0-qcom-multimedia-proprietary-image.zip",
+  ],
+  "iq-9075-evk": [
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-9075-evk/qli-2.0-qcom-multimedia-image.zip",
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-9075-evk/qli-2.0-qcom-multimedia-proprietary-image.zip",
+  ],
+  "iq-8275-evk": [
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-8275-evk/qli-2.0-qcom-multimedia-image.zip",
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-8275-evk/qli-2.0-qcom-multimedia-proprietary-image.zip",
+  ],
+  "iq-615-evk": [
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-615-evk/qli-2.0-qcom-multimedia-image.zip",
+    "https://artifacts.codelinaro.org/artifactory/qli-ci/flashable-binaries/meta-qcom/iq-615-evk/qli-2.0-qcom-multimedia-proprietary-image.zip",
+  ],
+}
