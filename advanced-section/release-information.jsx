1fa65b391532d4ad3fb11e4660f25f26d81007f7
Abin Joseph <abinjose@qti.qualcomm.com>
2026-06-08 17:12:20 +0530

    docs: add table for qsc-cli release parameters
    Add table documenting release parameters for qsc-cli.
    
    Signed-off-by: Abin Joseph <abinjose@qti.qualcomm.com>

--- advanced-section/release-information.jsx
+++ advanced-section/release-information.jsx
@@ -31,6 +31,29 @@ export const firmware_chipsets = [
   },
 ];
 
+export const qsc_release_parameters = [
+  {
+    product: "QCM6490.LE.2.0",
+    release: "r00125.1",
+    images: ["ADSP.HT.5.5.c8", "CDSP.HT.2.5.c3", "AOP.HO.3.6", "BOOT.MXF.1.0.c1", "TZ.XF.5.29"],
+  },
+  {
+    product: "QCS9100.LE.2.0",
+    release: "r00125.1",
+    images: ["DSP.AT.1.0.1", "AOP.HO.3.6.1", "BOOT.MXF.1.0.c1", "TZ.XF.5.29.1"],
+  },
+  {
+    product: "QCS8300.LE.2.0",
+    release: "r00125.1",
+    images: ["DSP.AT.1.0.1", "AOP.HO.3.6.1", "BOOT.MXF.1.0.c1", "TZ.XF.5.29.1"],
+  },
+  {
+    product: "QCS615.LE.2.0",
+    release: "r00125.1",
+    images: ["BOOT.MXF.1.0.c1", "AOP.HO.3.6.2", "TZ.XF.5.29.1", "ADSP.VT.5.2.c6", "CDSP.VT.2.2.c4"],
+  },
+];
+
 export const release_tags = [
   { label: "meta-qcom-release-tag", identifier: "qli-2.0" },
   { label: "meta-qcom-extras-release-tag", identifier: "qli-2.0" },
