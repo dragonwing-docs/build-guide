d386ea82f90ff4d98a8445a94d51a364bdf0647d
Abin Joseph <abinjose@qti.qualcomm.com>
2026-06-08 12:02:52 +0530

    docs: add chipset support information
    Add a table listing the supported chipsets for the build with
    firmware and build with qsc-cli section.
    
    Signed-off-by: Abin Joseph <abinjose@qti.qualcomm.com>

---
+++ advanced-section/release-information.jsx
@@ -0,0 +1,32 @@
+export const firmware_chipsets = [
+  {
+    label: "QCS6490",
+    description: "Qualcomm® RB3 Gen 2 Core Development Kit",
+    firmware_build_supported: true,
+    qsc_cli_supported: true,
+  },
+  {
+    label: "QCS5430",
+    description: "Qualcomm® RB3 Gen 2 Lite Core Development Kit",
+    firmware_build_supported: true,
+    qsc_cli_supported: true,
+  },
+  {
+    label: "QCS9075",
+    description: "Qualcomm® IQ-9075 EVK",
+    firmware_build_supported: true,
+    qsc_cli_supported: true,
+  },
+  {
+    label: "QCS8300",
+    description: "Qualcomm® IQ-8275 EVK",
+    firmware_build_supported: true,
+    qsc_cli_supported: true,
+  },
+  {
+    label: "QCS615",
+    description: "Qualcomm® IQ-615 EVK",
+    firmware_build_supported: true,
+    qsc_cli_supported: true,
+  },
+];
